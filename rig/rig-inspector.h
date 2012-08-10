a36a92cdbfd0b79ec23d97a84e9b29b90ffea8c5
Neil Roberts <neil@linux.intel.com>
2012-08-10 19:28:52 +0100

    Add RigInspector
    RigInspector represents a control which can edit all of the properties
    on a given object. The properties are introspected and appropriate
    controls for them are created at run-time.
    
    The inspector is constructed with a callback to actually update the
    property when a control is used. The callback is passed the source and
    target properties so that it can read the value before and after and
    copy it across.

---
+++ rig/rig-inspector.h
@@ -0,0 +1,26 @@
+#ifndef _RIG_INSPECTOR_H_
+#define _RIG_INSPECTOR_H_
+
+#include <cogl/cogl.h>
+
+#include "rig.h"
+
+extern RigType rig_inspector_type;
+
+typedef struct _RigInspector RigInspector;
+
+/* This is called whenever one of the properties changes */
+typedef void
+(* RigInspectorCallback) (RigProperty *target_property,
+                          RigProperty *source_property,
+                          void *user_data);
+
+#define RIG_INSPECTOR(x) ((RigInspector *) x)
+
+RigInspector *
+rig_inspector_new (RigContext *ctx,
+                   RigObject *object,
+                   RigInspectorCallback property_changed_cb,
+                   void *user_data);
+
+#endif /* _RIG_INSPECTOR_H_ */
