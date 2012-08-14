1829d7c50d5c8a5537a975f0a3e56275ba0e320b
Robert Bragg <robert@linux.intel.com>
2012-08-14 11:18:12 +0100

    Adds missing rig-property-types.h

---
+++ rig/rig-property-types.h
@@ -0,0 +1,15 @@
+
+SCALAR_TYPE(float, float, FLOAT)
+SCALAR_TYPE(double, double, DOUBLE)
+SCALAR_TYPE(integer, int, INTEGER)
+SCALAR_TYPE(enum, int, ENUM)
+SCALAR_TYPE(uint32, uint32_t, UINT32)
+SCALAR_TYPE(boolean, CoglBool, BOOLEAN)
+
+POINTER_TYPE(object, RigObject *, OBJECT)
+POINTER_TYPE(pointer, void *, POINTER)
+
+COMPOSITE_TYPE(quaternion, CoglQuaternion, QUATERNION)
+COMPOSITE_TYPE(color, RigColor, COLOR)
+
+ARRAY_TYPE(vec3, float, VEC3, 3)
