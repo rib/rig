67cad9c0eb5e2650b75aff16abde49f23aabd0cc
Robert Bragg <robert@linux.intel.com>
2012-11-08 22:14:18 +0000

    bitmap: ret CoglError from _new_with_malloc_buffer
    _cogl_bitmap_new_with_malloc_buffer() now takes a CoglError for throwing
    exceptional errors and all callers have been updated to pass through
    any application error pointer as appropriate.
    
    Reviewed-by: Neil Roberts <neil@linux.intel.com>

--- cogl/driver/gl/gles/cogl-texture-driver-gles.c
+++ cogl/driver/gl/gles/cogl-texture-driver-gles.c
@@ -204,7 +204,11 @@ _cogl_texture_driver_upload_subregion_to_gl (CoglContext *ctx,
       slice_bmp =
         _cogl_bitmap_new_with_malloc_buffer (ctx,
                                              width, height,
-                                             source_format);
+                                             source_format,
+                                             error);
+      if (!slice_bmp)
+        return FALSE;
+
       if (!_cogl_bitmap_copy_subregion (source_bmp,
                                         slice_bmp,
                                         src_x, src_y,
@@ -386,7 +390,10 @@ _cogl_texture_driver_upload_to_gl_3d (CoglContext *ctx,
       bmp = _cogl_bitmap_new_with_malloc_buffer (ctx,
                                                  bmp_width,
                                                  height,
-                                                 source_bmp_format);
+                                                 source_bmp_format,
+                                                 error);
+      if (!bmp)
+        return FALSE;
 
       for (i = 0; i < depth; i++)
         {
