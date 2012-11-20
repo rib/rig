rib/rig

commit afc5daab85e5faca99d6d6866658cb82c3954830

clip-stack: workaround intel gen6 viewport clip bug
The Intel Mesa gen6 driver doesn't currently handle scissoring offset
viewports correctly, so this implements a workaround to intersect the
current viewport bounds with the scissor rectangle.

cogl/cogl-framebuffer.c
@@ -111,6 +111,8 @@ _cogl_framebuffer_init (CoglFramebuffer *framebuffer,
   framebuffer->viewport_y = 0;
   framebuffer->viewport_width = width;
   framebuffer->viewport_height = height;
+  framebuffer->viewport_age = 0;
+  framebuffer->viewport_age_for_scissor_workaround = -1;
   framebuffer->dither_enabled = TRUE;
 
   framebuffer->modelview_stack = _cogl_matrix_stack_new ();
@@ -177,6 +179,9 @@ _cogl_framebuffer_free (CoglFramebuffer *framebuffer)
   framebuffer->projection_stack = NULL;
 
   cogl_object_unref (framebuffer->journal);
+
+  if (ctx->viewport_scissor_workaround_framebuffer == framebuffer)
+    ctx->viewport_scissor_workaround_framebuffer = NULL;
 
   ctx->framebuffers = g_list_remove (ctx->framebuffers, framebuffer);
 
@@ -457,6 +462,7 @@ cogl_framebuffer_set_viewport (CoglFramebuffer *framebuffer,
   framebuffer->viewport_y = y;
   framebuffer->viewport_width = width;
   framebuffer->viewport_height = height;
+  framebuffer->viewport_age++;
 
   if (framebuffer->context->current_draw_buffer == framebuffer)
     framebuffer->context->current_draw_buffer_changes |=

cogl/cogl-clip-stack.c
@@ -446,10 +446,14 @@ _cogl_clip_stack_get_bounds (CoglClipStack *stack,
     {
       /* Get the intersection of the current scissor and the bounding
          box of this clip */
-      *scissor_x0 = MAX (*scissor_x0, entry->bounds_x0);
-      *scissor_y0 = MAX (*scissor_y0, entry->bounds_y0);
-      *scissor_x1 = MIN (*scissor_x1, entry->bounds_x1);
-      *scissor_y1 = MIN (*scissor_y1, entry->bounds_y1);
+      _cogl_util_scissor_intersect (entry->bounds_x0,
+                                    entry->bounds_y0,
+                                    entry->bounds_x1,
+                                    entry->bounds_y1,
+                                    scissor_x0,
+                                    scissor_y0,
+                                    scissor_x1,
+                                    scissor_y1);
     }
 }
 

cogl/cogl-context-private.h
@@ -81,6 +81,9 @@ struct _CoglContext
   unsigned long features[COGL_FLAGS_N_LONGS_FOR_SIZE (_COGL_N_FEATURE_IDS)];
   CoglPrivateFeatureFlags private_feature_flags;
 
+  CoglBool needs_viewport_scissor_workaround;
+  CoglFramebuffer *viewport_scissor_workaround_framebuffer;
+
   CoglPipeline *default_pipeline;
   CoglPipelineLayer *default_layer_0;
   CoglPipelineLayer *default_layer_n;

cogl/cogl-context.c
@@ -52,6 +52,7 @@
 #include "cogl-error-private.h"
 
 #include <string.h>
+#include <stdlib.h>
 
 #ifdef HAVE_COGL_GL
 #include "cogl-pipeline-fragend-arbfp-private.h"
@@ -228,6 +229,22 @@ cogl_context_new (CoglDisplay *display,
 
   /* Initialise the driver specific state */
   _cogl_init_feature_overrides (context);
+
+  /* XXX: ONGOING BUG: Intel viewport scissor
+   *
+   * Intel gen6 drivers don't currently correctly handle offset
+   * viewports, since primitives aren't clipped within the bounds of
+   * the viewport.  To workaround this we push our own clip for the
+   * viewport that will use scissoring to ensure we clip as expected.
+   *
+   * TODO: file a bug upstream!
+   */
+  if (context->gpu.driver_package == COGL_GPU_INFO_DRIVER_PACKAGE_MESA &&
+      context->gpu.architecture == COGL_GPU_INFO_ARCHITECTURE_SANDYBRIDGE &&
+      !getenv ("COGL_DISABLE_INTEL_VIEWPORT_SCISSORT_WORKAROUND"))
+    context->needs_viewport_scissor_workaround = TRUE;
+  else
+    context->needs_viewport_scissor_workaround = FALSE;
 
   context->sampler_cache = _cogl_sampler_cache_new (context);
 

cogl/cogl-framebuffer-private.h
@@ -127,6 +127,8 @@ struct _CoglFramebuffer
   float               viewport_y;
   float               viewport_width;
   float               viewport_height;
+  int                 viewport_age;
+  int                 viewport_age_for_scissor_workaround;
 
   CoglClipState       clip_state;
 

cogl/cogl-util.h
@@ -278,4 +278,20 @@ _cogl_util_memmem (const void *haystack,
                    size_t needle_len);
 #endif
 
+static inline void
+_cogl_util_scissor_intersect (int rect_x0,
+                              int rect_y0,
+                              int rect_x1,
+                              int rect_y1,
+                              int *scissor_x0,
+                              int *scissor_y0,
+                              int *scissor_x1,
+                              int *scissor_y1)
+{
+  *scissor_x0 = MAX (*scissor_x0, rect_x0);
+  *scissor_y0 = MAX (*scissor_y0, rect_y0);
+  *scissor_x1 = MIN (*scissor_x1, rect_x1);
+  *scissor_y1 = MIN (*scissor_y1, rect_y1);
+}
+
 #endif /* __COGL_UTIL_H */

cogl/driver/gl/cogl-clip-stack-gl.c
@@ -466,7 +466,12 @@ _cogl_clip_stack_gl_flush (CoglClipStack *stack,
      anything */
   if (ctx->current_clip_stack_valid)
     {
-      if (ctx->current_clip_stack == stack)
+      if (ctx->current_clip_stack == stack &&
+          (ctx->needs_viewport_scissor_workaround == FALSE ||
+           (framebuffer->viewport_age ==
+            framebuffer->viewport_age_for_scissor_workaround &&
+            ctx->viewport_scissor_workaround_framebuffer ==
+            framebuffer)))
         return;
 
       _cogl_clip_stack_unref (ctx->current_clip_stack);
@@ -482,8 +487,11 @@ _cogl_clip_stack_gl_flush (CoglClipStack *stack,
     disable_clip_planes (ctx);
   GE( ctx, glDisable (GL_STENCIL_TEST) );
 
-  /* If the stack is empty then there's nothing else to do */
-  if (stack == NULL)
+  /* If the stack is empty then there's nothing else to do
+   *
+   * See comment below about ctx->needs_viewport_scissor_workaround
+   */
+  if (stack == NULL && !ctx->needs_viewport_scissor_workaround)
     {
       COGL_NOTE (CLIPPING, "Flushed empty clip stack");
 
@@ -499,6 +507,31 @@ _cogl_clip_stack_gl_flush (CoglClipStack *stack,
   _cogl_clip_stack_get_bounds (stack,
                                &scissor_x0, &scissor_y0,
                                &scissor_x1, &scissor_y1);
+
+  /* XXX: ONGOING BUG: Intel viewport scissor
+   *
+   * Intel gen6 drivers don't correctly handle offset viewports, since
+   * primitives aren't clipped within the bounds of the viewport.  To
+   * workaround this we push our own clip for the viewport that will
+   * use scissoring to ensure we clip as expected.
+   *
+   * TODO: file a bug upstream!
+   */
+  if (ctx->needs_viewport_scissor_workaround)
+    {
+      _cogl_util_scissor_intersect (framebuffer->viewport_x,
+                                    framebuffer->viewport_y,
+                                    framebuffer->viewport_x +
+                                      framebuffer->viewport_width,
+                                    framebuffer->viewport_y +
+                                      framebuffer->viewport_height,
+                                    &scissor_x0, &scissor_y0,
+                                    &scissor_x1, &scissor_y1);
+      framebuffer->viewport_age_for_scissor_workaround =
+        framebuffer->viewport_age;
+      ctx->viewport_scissor_workaround_framebuffer =
+        framebuffer;
+    }
 
   /* Enable scissoring as soon as possible */
   if (scissor_x0 >= scissor_x1 || scissor_y0 >= scissor_y1)

commit 0dca99ddf728c8d4e3003861a03e8a2beccf282d

Fix flushing the stencil viewport clipping workaround
There were two problems with the stencil viewport clip workaround
introduced in afc5daab8:

• When the viewport is changed the current clip state is not marked as
  dirty. That means that when the framebuffer state is next flushed it
  would continue to use the stencil from the previous viewport.

• When the viewport is automatically updated due to the window being
  resized the viewport age was not incremented so the clip state
  wouldn't be flushed.

I noticed the bugs by running cogl-sdl2-hello.

This patch makes it so that the clip state is dirtied in
cogl_framebuffer_set_viewport if the workaround is enabled.

The automatic viewport changing code now just calls
cogl_framebuffer_set_viewport instead of directly prodding the
viewport values. This has the side-effect that it will also cause the
journal to be flushed. This seems like the right thing to do anyway
and presumably there would have been a bug before where it wouldn't
have flushed the journal, although presumably this is extremely
unlikely because it would have to have done a resize in the middle of
painting the scene.

Reviewed-by: Robert Bragg <robert@linux.intel.com>

cogl/cogl-framebuffer.c
@@ -448,6 +448,8 @@ cogl_framebuffer_set_viewport (CoglFramebuffer *framebuffer,
                                float width,
                                float height)
 {
+  CoglContext *context = framebuffer->context;
+
   _COGL_RETURN_IF_FAIL (width > 0 && height > 0);
 
   if (framebuffer->viewport_x == x &&
@@ -464,9 +466,13 @@ cogl_framebuffer_set_viewport (CoglFramebuffer *framebuffer,
   framebuffer->viewport_height = height;
   framebuffer->viewport_age++;
 
-  if (framebuffer->context->current_draw_buffer == framebuffer)
-    framebuffer->context->current_draw_buffer_changes |=
-      COGL_FRAMEBUFFER_STATE_VIEWPORT;
+  if (context->current_draw_buffer == framebuffer)
+    {
+      context->current_draw_buffer_changes |= COGL_FRAMEBUFFER_STATE_VIEWPORT;
+
+      if (context->needs_viewport_scissor_workaround)
+        context->current_draw_buffer_changes |= COGL_FRAMEBUFFER_STATE_CLIP;
+    }
 }
 
 float

cogl/cogl-onscreen.c
@@ -387,17 +387,7 @@ _cogl_framebuffer_winsys_update_size (CoglFramebuffer *framebuffer,
   framebuffer->width = width;
   framebuffer->height = height;
 
-  framebuffer->viewport_x = 0;
-  framebuffer->viewport_y = 0;
-  framebuffer->viewport_width = width;
-  framebuffer->viewport_height = height;
-
-  /* If the framebuffer being updated is the current framebuffer we
-   * mark the viewport state as changed so it will be updated the next
-   * time _cogl_framebuffer_flush_state() is called. */
-  if (framebuffer->context->current_draw_buffer == framebuffer)
-    framebuffer->context->current_draw_buffer_changes |=
-      COGL_FRAMEBUFFER_STATE_VIEWPORT;
+  cogl_framebuffer_set_viewport (framebuffer, 0, 0, width, height);
 }
 
 void