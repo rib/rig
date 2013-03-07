c935b725adc908bf81a911bceeab3ece1586a37b
Robert Bragg <robert@linux.intel.com>
2013-03-07 00:13:38 +0000

    entry: add rut_entry_set_icon api
    This adds a rut_entry_set_icon function that lets us show an icon at the
    left of a text entry. This will be used for example to show a search
    icon for search entries.

--- rut/rut-entry.h
+++ rut/rut-entry.h
@@ -4,6 +4,8 @@
 
 #include "rut.h"
 
+#include "rut-icon.h"
+
 extern RutType rut_entry_type;
 typedef struct _RutEntry RutEntry;
 #define RUT_ENTRY(x) ((RutEntry *) x)
@@ -12,7 +14,7 @@ RutEntry *
 rut_entry_new (RutContext *context);
 
 void
-rut_entry_set_size (RutEntry *entry,
+rut_entry_set_size (RutObject *entry,
                     float width,
                     float height);
 
@@ -25,11 +27,15 @@ rut_entry_set_height (RutObject *entry,
                       float height);
 
 void
-rut_entry_get_size (RutEntry *entry,
+rut_entry_get_size (RutObject *entry,
                     float *width,
                     float *height);
 
 RutText *
 rut_entry_get_text (RutEntry *entry);
+
+void
+rut_entry_set_icon (RutEntry *entry,
+                    RutIcon *icon);
 
 #endif /* _RUT_ENTRY_H_ */
