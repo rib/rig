6559f6fb084e6abfd67a1a4eeff32da737dd1a43
Robert Bragg <robert@linux.intel.com>
2014-01-03 14:28:48 +0000

    stash

--- rig/rig-device.c
+++ rig/rig-device.c
@@ -93,7 +93,7 @@ rig_device_paint (RutShell *shell, void *user_data)
                              NULL);
 
 #warning "fixme: don't dispatch input events directly in the device process"
-  rut_shell_dispatch_input_events (shell);
+  //rut_shell_dispatch_input_events (shell);
   //rut_shell_clear_input_queue (shell);
 
   rut_shell_run_pre_paint_callbacks (shell);
