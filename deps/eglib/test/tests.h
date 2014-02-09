3f92564e070504f90d34589fc3fbbe8581434b21
Robert Bragg <robert@linux.intel.com>
2014-02-09 15:06:14 +0000

    eglib: remove unused gpattern and gmarkup apis
    This simply removes a couple of eglib apis that aren't needed by Cogl
    and the corresponding tests.

--- deps/eglib/test/tests.h
+++ deps/eglib/test/tests.h
@@ -15,17 +15,15 @@ DEFINE_TEST_GROUP_INIT_H(shell_tests_init);
 DEFINE_TEST_GROUP_INIT_H(spawn_tests_init);
 DEFINE_TEST_GROUP_INIT_H(timer_tests_init);
 DEFINE_TEST_GROUP_INIT_H(file_tests_init);
-DEFINE_TEST_GROUP_INIT_H(pattern_tests_init);
 DEFINE_TEST_GROUP_INIT_H(dir_tests_init);
-DEFINE_TEST_GROUP_INIT_H(markup_tests_init);
 DEFINE_TEST_GROUP_INIT_H(unicode_tests_init);
 DEFINE_TEST_GROUP_INIT_H(utf8_tests_init);
 DEFINE_TEST_GROUP_INIT_H(endian_tests_init);
 DEFINE_TEST_GROUP_INIT_H(module_tests_init);
 DEFINE_TEST_GROUP_INIT_H(memory_tests_init);
 
-static Group test_groups [] = {	
-	{"string",    string_tests_init}, 
+static Group test_groups [] = {
+	{"string",    string_tests_init},
 	{"strutil",   strutil_tests_init},
 	{"ptrarray",  ptrarray_tests_init},
 	{"slist",     slist_tests_init},
@@ -37,8 +35,7 @@ static Group test_groups [] = {
 	{"queue",     queue_tests_init},
 	{"path",      path_tests_init},
 	{"shell",     shell_tests_init},
-	{"markup",    markup_tests_init},
-#if !DISABLE_PROCESS_TESTS 
+#if !DISABLE_PROCESS_TESTS
 	{"spawn",     spawn_tests_init},
 	{"module",    module_tests_init},
 #endif
@@ -46,7 +43,6 @@ static Group test_groups [] = {
 	{"file",      file_tests_init},
 #endif
 	{"timer",     timer_tests_init},
-	{"pattern",   pattern_tests_init},
 	{"dir",       dir_tests_init},
 	{"unicode",   unicode_tests_init},
 	{"utf8",      utf8_tests_init},
