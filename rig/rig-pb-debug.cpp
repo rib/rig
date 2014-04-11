d8a12c668483559227382cb8d98ff51f36c7ddeb
Robert Bragg <robert.bragg@intel.com>
2014-04-11 03:09:32 +0100

    Adds some debug apis for pretty printing protobuf data

---
+++ rig/rig-pb-debug.cpp
@@ -0,0 +1,111 @@
+/*
+ * Rig
+ *
+ * UI Engine & Editor
+ *
+ * Copyright (C) 2014  Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <config.h>
+
+#include <glib.h>
+
+#include <google/protobuf/text_format.h>
+#include <google/protobuf/message.h>
+G_BEGIN_DECLS
+#define _Static_assert(X, Y)
+#include <rig.pb-c.h>
+#undef _Static_assert
+G_END_DECLS
+
+#include <rig.pb.h>
+
+G_BEGIN_DECLS
+
+void
+rig_pb_print_ui (const Rig__UI *pb_ui)
+{
+  size_t len = protobuf_c_message_get_packed_size ((ProtobufCMessage *)pb_ui);
+  unsigned char *buf = (unsigned char *)g_malloc (len);
+  protobuf_c_message_pack ((ProtobufCMessage *)pb_ui, buf);
+
+  rig::UI ui;
+  if (!ui.ParseFromArray(buf, len))
+    {
+      g_warning ("Failed to parse UI for printing");
+      return;
+    }
+
+  std::string ui_str;
+  google::protobuf::TextFormat::PrintToString(ui, &ui_str);
+
+  g_print (ui_str.c_str());
+
+  g_free (buf);
+}
+
+void
+rig_pb_print_frame_setup (const Rig__FrameSetup *pb_frame_setup)
+{
+  size_t len = protobuf_c_message_get_packed_size ((ProtobufCMessage *)pb_frame_setup);
+  unsigned char *buf = (unsigned char *)g_malloc (len);
+  protobuf_c_message_pack ((ProtobufCMessage *)pb_frame_setup, buf);
+
+  rig::FrameSetup frame_setup;
+  if (!frame_setup.ParseFromArray(buf, len))
+    {
+      g_warning ("Failed to parse FrameSetup for printing");
+      return;
+    }
+
+  std::string ui_str;
+  google::protobuf::TextFormat::PrintToString(frame_setup, &ui_str);
+
+  g_print (ui_str.c_str());
+
+  g_free (buf);
+}
+
+void
+rig_pb_print_ui_diff (const Rig__UIDiff *pb_ui_diff)
+{
+  size_t len = protobuf_c_message_get_packed_size ((ProtobufCMessage *)pb_ui_diff);
+  unsigned char *buf = (unsigned char *)g_malloc (len);
+  protobuf_c_message_pack ((ProtobufCMessage *)pb_ui_diff, buf);
+
+  rig::UIDiff ui_diff;
+  if (!ui_diff.ParseFromArray(buf, len))
+    {
+      g_warning ("Failed to parse FrameSetup for printing");
+      return;
+    }
+
+  std::string ui_str;
+  google::protobuf::TextFormat::PrintToString(ui_diff, &ui_str);
+
+  g_print (ui_str.c_str());
+
+  g_free (buf);
+}
+
+G_END_DECLS
---
+++ rig/rig-pb-debug.h
@@ -0,0 +1,49 @@
+/*
+ * Rig
+ *
+ * UI Engine & Editor
+ *
+ * Copyright (C) 2014  Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef __RIG_PB_DEBUG_H__
+#define __RIG_PB_DEBUG_H__
+
+#include <glib.h>
+
+#include "rig.pb-c.h"
+
+G_BEGIN_DECLS
+
+void
+rig_pb_print_ui (const Rig__UI *pb_ui);
+
+void
+rig_pb_print_frame_setup (const Rig__FrameSetup *pb_frame_setup);
+
+void
+rig_pb_print_ui_diff (const Rig__UIDiff *pb_ui_diff);
+
+G_END_DECLS
+
+#endif /* __RIG_PB_DEBUG_H__ */
