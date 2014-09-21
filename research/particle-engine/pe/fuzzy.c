c764dddfdf88888258a0b9d7b9394c22a98e484e
Robert Bragg <robert@sixbynine.org>
2014-09-21 23:40:25 +0100

    fuzzy: request random floats + fix pseudo gaussian distribution apis

--- research/particle-engine/pe/fuzzy.c
+++ research/particle-engine/pe/fuzzy.c
@@ -28,6 +28,8 @@
 
 #include "fuzzy.h"
 
+#define IRWIN_HALL_SUM_LEN 12
+
 float fuzzy_float_get_real_value(struct fuzzy_float *variance,
                                  c_rand_t *rand)
 {
@@ -35,32 +37,29 @@ float fuzzy_float_get_real_value(struct fuzzy_float *variance,
     case FLOAT_VARIANCE_LINEAR:
     {
         float v = variance->variance / 2;
-        return (float)c_rand_double_range(rand,
-                                          variance->value - v,
-                                          variance->value + v);
+        return c_rand_float_range(rand,
+                                  variance->value - v,
+                                  variance->value + v);
     }
     case FLOAT_VARIANCE_PROPORTIONAL:
     {
         float v = variance->value * variance->variance;
-        return (float)c_rand_double_range(rand,
-                                          variance->value - v,
-                                          variance->value + v);
+        return c_rand_float_range(rand,
+                                  variance->value - v,
+                                  variance->value + v);
     }
     case FLOAT_VARIANCE_IRWIN_HALL:
     {
-        int i, n = 12;
-        float u = 0, v;
+        float v = variance->variance / 2;
+        float f = 0;
+        int i;
 
-        for (i = 0; i < n; i++)
-            u += (float)c_rand_double_range(rand, 0.0f, 1.0f);
+        for (i = 0; i < IRWIN_HALL_SUM_LEN; i++)
+            f += (float)c_rand_float_range(rand,
+                                           variance->value - v,
+                                           variance->value + v);
 
-        u -= n / 2;
-        u /= n / 2;
-
-        v = variance->variance * u;
-        return (float)c_rand_double_range(rand,
-                                          variance->value - v,
-                                          variance->value + v);
+        return f / 12.0f;
     }
     case FLOAT_VARIANCE_NONE:
     default:
@@ -68,20 +67,20 @@ float fuzzy_float_get_real_value(struct fuzzy_float *variance,
     }
 }
 
-gdouble fuzzy_double_get_real_value(struct fuzzy_double *variance,
-                                    c_rand_t *rand)
+double fuzzy_double_get_real_value(struct fuzzy_double *variance,
+                                   c_rand_t *rand)
 {
     switch (variance->type) {
     case DOUBLE_VARIANCE_LINEAR:
     {
-        gdouble v = variance->variance / 2;
+        double v = variance->variance / 2;
         return c_rand_double_range(rand,
                                    variance->value - v,
                                    variance->value + v);
     }
     case DOUBLE_VARIANCE_PROPORTIONAL:
     {
-        gdouble v = variance->value * variance->variance;
+        double v = variance->value * variance->variance;
         return c_rand_double_range(rand,
                                    variance->value - v,
                                    variance->value + v);
@@ -103,35 +102,33 @@ void fuzzy_vector_get_real_value(struct fuzzy_vector *variance,
         for (i = 0; i < C_N_ELEMENTS(variance->value); i++) {
             float v = variance->variance[i] / 2;
 
-            value[i] = (float)c_rand_double_range(rand,
-                                                  variance->value[i] - v,
-                                                  variance->value[i] + v);
+            value[i] = c_rand_float_range(rand,
+                                          variance->value[i] - v,
+                                          variance->value[i] + v);
         }
         break;
     case VECTOR_VARIANCE_PROPORTIONAL:
         for (i = 0; i < C_N_ELEMENTS(variance->value); i++) {
             float v = variance->value[i] * variance->variance[i];
 
-            value[i] = (float)c_rand_double_range(rand,
-                                                  variance->value[i] - v,
-                                                  variance->value[i] + v);
+            value[i] = c_rand_float_range(rand,
+                                          variance->value[i] - v,
+                                          variance->value[i] + v);
         }
         break;
     case VECTOR_VARIANCE_IRWIN_HALL:
         for(i = 0; i < C_N_ELEMENTS(variance->value); i++) {
-            int j, n = 12;
-            float u = 0, v;
+            float v = variance->variance[i] / 2;
+            float f = 0;
+            int j;
 
-            for (j = 0; j < n; j++)
-                u += (float)c_rand_double_range(rand, 0.0f, 1.0f);
+            for (j = 0; j < IRWIN_HALL_SUM_LEN; j++) {
+                f += c_rand_float_range(rand,
+                                        variance->value[i] - v,
+                                        variance->value[i] + v);
+            }
 
-            u -= n / 2;
-            u /= n / 2;
-
-            v = variance->variance[i] * u;
-            value[i]= (float)c_rand_double_range(rand,
-                                                 variance->value[i] - v,
-                                                 variance->value[i] + v);
+            value[i] = f / 12.0f;
         }
         break;
     case VECTOR_VARIANCE_NONE:
