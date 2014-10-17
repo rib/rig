57cf857a560c4ad2f99de58c1eb7ee92b09c1424
Robert Bragg <robert@sixbynine.org>
2014-10-18 00:31:33 +0100

    rut: Add Android.mk

---
+++ rut/Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := rut
+LOCAL_CFLAGS := -DRIG_ENABLE_DEBUG
+LOCAL_SRC_FILES := $(wildcard $(LOCAL_PATH)/*.c)
+LOCAL_EXPORT_CFLAGS := -I$(LOCAL_PATH)
+
+LOCAL_STATIC_LIBRARIES := clib cogl android_native_app_glue
+LOCAL_SHARED_LIBRARIES := libuv
+
+include $(BUILD_STATIC_LIBRARY)
+
+$(call import-module, libuv)
+$(call import-module, android/native_app_glue)
