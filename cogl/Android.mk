1fd4cea82e720ce6bc1abb93f614b7025e7d96cf
Robert Bragg <robert@sixbynine.org>
2014-10-18 00:31:50 +0100

    cogl: Add Android.mk

---
+++ cogl/Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := cogl
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../ $(LOCAL_PATH) $(LOCAL_PATH)/driver $(LOCAL_PATH)/driver/nop $(LOCAL_PATH)/driver/gl $(LOCAL_PATH)/winsys
+LOCAL_CFLAGS := -DHAVE_CONFIG_H -DCG_COMPILATION -DCG_GLES2_LIBNAME=\"libGLESv3.so\" -DCG_ENABLE_DEBUG -DCG_GL_DEBUG -DCG_OBJECT_DEBUG
+LOCAL_SRC_FILES := \
+    $(wildcard $(LOCAL_PATH)/*.c) \
+    $(wildcard $(LOCAL_PATH)/winsys/*.c) \
+    $(wildcard $(LOCAL_PATH)/drivers/nop/*.c) \
+    $(wildcard $(LOCAL_PATH)/drivers/gl/*.c) \
+    $(wildcard $(LOCAL_PATH)/drivers/gl/gl/*.c) \
+    $(wildcard $(LOCAL_PATH)/drivers/gl/gles*.c)
+
+LOCAL_EXPORT_CFLAGS := -I$(LOCAL_PATH)/../
+
+LOCAL_STATIC_LIBRARIES := clib
+LOCAL_SHARED_LIBRARIES := libuv
+
+include $(BUILD_STATIC_LIBRARY)
+
+$(call import-module, libuv)
