a3a044fde830434430f808d8dbb3475ef17104bd
Robert Bragg <robert@sixbynine.org>
2014-12-28 12:30:11 +0000

    tests: use C_STATIC_ASSERT

--- tests/conform/test-version.c
+++ tests/conform/test-version.c
@@ -4,7 +4,7 @@
 
 #include "test-utils.h"
 
-/* So we can use _CG_STATIC_ASSERT we include the internal
+/* So we can use _C_STATIC_ASSERT we include the internal
  * cogl-util.h header. Since internal headers explicitly guard against
  * applications including them directly instead of including
  * <cogl/cogl.h> we define __CG_H_INSIDE__ here to subvert those
@@ -13,54 +13,54 @@
 #include <cogl/cogl-util.h>
 #undef __CG_H_INSIDE__
 
-_CG_STATIC_ASSERT (CG_VERSION_ENCODE (CG_VERSION_MAJOR,
+_C_STATIC_ASSERT (CG_VERSION_ENCODE (CG_VERSION_MAJOR,
                                           CG_VERSION_MINOR,
                                           CG_VERSION_MICRO) ==
                      CG_VERSION,
                      "The pre-encoded Cogl version does not match the version "
                      "encoding macro");
 
-_CG_STATIC_ASSERT (CG_VERSION_GET_MAJOR (CG_VERSION_ENCODE (100,
+_C_STATIC_ASSERT (CG_VERSION_GET_MAJOR (CG_VERSION_ENCODE (100,
                                                                   200,
                                                                   300)) ==
                      100,
                      "Getting the major component out of a encoded version "
                      "does not work");
-_CG_STATIC_ASSERT (CG_VERSION_GET_MINOR (CG_VERSION_ENCODE (100,
+_C_STATIC_ASSERT (CG_VERSION_GET_MINOR (CG_VERSION_ENCODE (100,
                                                                   200,
                                                                   300)) ==
                      200,
                      "Getting the minor component out of a encoded version "
                      "does not work");
-_CG_STATIC_ASSERT (CG_VERSION_GET_MICRO (CG_VERSION_ENCODE (100,
+_C_STATIC_ASSERT (CG_VERSION_GET_MICRO (CG_VERSION_ENCODE (100,
                                                                   200,
                                                                   300)) ==
                      300,
                      "Getting the micro component out of a encoded version "
                      "does not work");
 
-_CG_STATIC_ASSERT (CG_VERSION_CHECK (CG_VERSION_MAJOR,
+_C_STATIC_ASSERT (CG_VERSION_CHECK (CG_VERSION_MAJOR,
                                          CG_VERSION_MINOR,
                                          CG_VERSION_MICRO),
                      "Checking the Cogl version against the current version "
                      "does not pass");
-_CG_STATIC_ASSERT (!CG_VERSION_CHECK (CG_VERSION_MAJOR,
+_C_STATIC_ASSERT (!CG_VERSION_CHECK (CG_VERSION_MAJOR,
                                           CG_VERSION_MINOR,
                                           CG_VERSION_MICRO + 1),
                      "Checking the Cogl version against a later micro version "
                      "should not pass");
-_CG_STATIC_ASSERT (!CG_VERSION_CHECK (CG_VERSION_MAJOR,
+_C_STATIC_ASSERT (!CG_VERSION_CHECK (CG_VERSION_MAJOR,
                                           CG_VERSION_MINOR + 1,
                                           CG_VERSION_MICRO),
                      "Checking the Cogl version against a later minor version "
                      "should not pass");
-_CG_STATIC_ASSERT (!CG_VERSION_CHECK (CG_VERSION_MAJOR + 1,
+_C_STATIC_ASSERT (!CG_VERSION_CHECK (CG_VERSION_MAJOR + 1,
                                           CG_VERSION_MINOR,
                                           CG_VERSION_MICRO),
                      "Checking the Cogl version against a later major version "
                      "should not pass");
 
-_CG_STATIC_ASSERT (CG_VERSION_CHECK (CG_VERSION_MAJOR - 1,
+_C_STATIC_ASSERT (CG_VERSION_CHECK (CG_VERSION_MAJOR - 1,
                                          CG_VERSION_MINOR,
                                          CG_VERSION_MICRO),
                      "Checking the Cogl version against a older major version "
