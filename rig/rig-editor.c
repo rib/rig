38835ed1517a7e4694d12799a42e74bece1a9106
Robert Bragg <robert@sixbynine.org>
2015-03-01 19:39:13 +0000

    editor: associated assets with ui for saving

--- rig/rig-editor.c
+++ rig/rig-editor.c
@@ -669,6 +669,10 @@ apply_asset_input_with_entity(rig_editor_t *editor,
         break;
     }
 
+    rut_object_ref(asset);
+    engine->edit_mode_ui->assets =
+        c_llist_prepend(engine->edit_mode_ui->assets, asset);
+
     sub_journal = rig_editor_pop_undo_subjournal(editor);
 
     if (rig_undo_journal_is_empty(sub_journal))
